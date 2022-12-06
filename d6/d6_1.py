6f23b7036a241a89ce1cea1ae35d416371c575aa
Juan Cabrera <juan.cabrera@tu-dresden.de>
2022-12-06 08:40:24 +0100

    ajancio

--- d6/d6_1.py
+++ d6/d6_1.py
@@ -5,8 +5,6 @@ lines = f.readlines()
 
 input = lines[0]
 
-# print(input[0:0+4])
-
 for i in range(len(input)):
     if len(set(input[i:i+4])) == 4:
         print(i + 4)
--- d6/d6_2.py
+++ d6/d6_2.py
@@ -5,8 +5,6 @@ lines = f.readlines()
 
 input = lines[0]
 
-# print(input[0:0+4])
-
 for i in range(len(input)):
     if len(set(input[i:i+14])) == 14:
         print(i + 14)
